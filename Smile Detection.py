ab6ba281faa7347a707bd770b6a969ca61da04b9
mehrnooshazizi <61627074+mehrnooshazizi@users.noreply.github.com>
2020-10-09 20:21:17 +0330

    update

--- Smile Detection.py
+++ Smile Detection.py
@@ -7,22 +7,21 @@ Created on Fri Oct  9 19:30:27 2020
 import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 face_smile = cv2.CascadeClassifier('haarcascade_smile.xml')
-
 cam = cv2.VideoCapture(0)
 counter=1
 while True:
     success, frame = cam.read()
     if success == True:
-        resized_frame = cv2.resize(frame,(600,400))
-        gray = cv2.cvtColor(resized_frame,cv2.COLOR_BGR2GRAY)
+        #resized_frame = cv2.resize(frame,(600,400))
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)  
         for (x,y,w,h) in faces:
-            newframe = cv2.rectangle(resized_frame,(x,y),(x + w, y + h),(0,255,0),2)           
-            croped_frame = resized_frame[y: y + h, x: x + w]
-            smiles = face_smile.detectMultiScale(croped_frame , 1.3, 6)             
+            newframe = cv2.rectangle(frame,(x,y),(x + w, y + h),(0,255,0),2)           
+            croped_frame = frame[y: y + h, x: x + w]
+            smiles = face_smile.detectMultiScale(croped_frame,scaleFactor=1.7,minNeighbors=22,minSize=(25, 25),)          
             for (xs,ys,ws,hs) in smiles:
-                frame2 = cv2.rectangle(newframe,(xs+x,ys+y),(xs + ws+x, ys + hs+y),(0,0,255),2)
-                croped_frame1 = resized_frame[y: ys + hs+y, x: xs + ws+x]
+                frame2 = cv2.rectangle(croped_frame,(xs,ys),(xs+ ws, ys+ hs),(0,0,255),2)
+                croped_frame1 = frame[y: ys+ hs, x: xs+ ws]
                 cv2.imwrite(f'G:/mehrnoosh/smile{counter}.jpg',croped_frame1)
                 counter+=1
             cv2.imshow('myimage', frame)
